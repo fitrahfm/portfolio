dc7069165c7f7ad182edd2899a4b64c9abd2e04c
Muhammad Fitrah Munir <fitrahfm@Muhammads-MacBook-Pro.local>
2023-06-21 16:46:37 +0800

    add auto scroll

--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -9,6 +9,12 @@ const ThemeSwitch: FunctionComponent<IProps> = ({ theme }) => {
   useEffect(() => {
     const HtmlElement = document.body.parentElement;
     HtmlElement!.className = `fm-${theme}`;
+
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
   }, [theme]);
 
   return <div></div>;
