cac71934c331a780bab518c09803d9837b5e3559
Muhammad Fitrah Munir <fitrahfm@Muhammads-MacBook-Pro.local>
2023-06-22 16:32:47 +0800

    fix:add timezone in theme-changer helper

--- src/helpers/theme-changer.tsx
+++ src/helpers/theme-changer.tsx
@@ -1,3 +1,9 @@
+const TIMEZONE = "Asia/Jakarta";
 const HOUR = 15;
 
-export const theme = new Date().getHours() < HOUR ? "light" : "dark";
+export const theme =
+  new Date(
+    new Date().toLocaleString("en-US", { timeZone: TIMEZONE })
+  ).getHours() < HOUR
+    ? "light"
+    : "dark";
