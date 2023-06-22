74b0ab94d942faa42660b41d152d51f36d336dba
Muhammad Fitrah Munir <fitrahfm@Muhammads-MacBook-Pro.local>
2023-06-22 10:59:05 +0800

    feat:set time to 3pm for switching to dark mode

--- src/helpers/theme-changer.tsx
+++ src/helpers/theme-changer.tsx
@@ -1,6 +1,6 @@
 import { prefix } from "tailwind.config";
 
-const HOUR = 9;
+const HOUR = 15;
 
 export const theme = new Date().getHours() < HOUR ? "light" : "dark";
 export const themeWithPrefix = prefix + theme;
