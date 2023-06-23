81d64e0909894103460be69e5be8c985220744c6
Muhammad Fitrah Munir <fitrahfm@Muhammads-MacBook-Pro.local>
2023-06-23 21:25:05 +0800

    test datetime on server

--- src/helpers/theme-changer.tsx
+++ src/helpers/theme-changer.tsx
@@ -1,6 +1,14 @@
 const TIMEZONE = "Asia/Jakarta";
 const HOUR = 15;
 
+console.log(
+  new Date(
+    new Date().toLocaleString("en-US", { timeZone: TIMEZONE })
+  ).getHours() < HOUR
+    ? "light"
+    : "dark"
+);
+
 export const theme =
   new Date(
     new Date().toLocaleString("en-US", { timeZone: TIMEZONE })
